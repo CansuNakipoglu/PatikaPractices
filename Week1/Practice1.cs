164c8b40b899ed72fe0af8ed564f24484174c3dd
cansunakipoglu <cansuunakipoglu@gmail.com>
2024-07-17 13:33:12 +0300

    Uyuglamaları kolay bir şekilde çalıştırmak için alt yapı hazırlandı.

---
+++ Week1/Practice1.cs
@@ -0,0 +1,9 @@
+namespace Week1;
+
+public static class Practice1
+{
+    public static void ExecPractice()
+    {
+        Console.WriteLine("Cansu Nakipoğlu, Patika'nın uzman kadrosuyla kendimi geliştirmek istiyorum!");
+    }
+}
---
+++ Week1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Week1;
+
+Console.Write("Lütfen Çalıştırmak İstediğiniz Uygulamanın Numarasını Giriniz: ");
+var selection = Console.ReadLine();
+
+switch (selection)
+{
+    case "1": Practice1.ExecPractice();
+        break;
+    default: Console.WriteLine("Lütfen Geçerli Bir Uygulama Numarası Giriniz");
+        break;
+}
