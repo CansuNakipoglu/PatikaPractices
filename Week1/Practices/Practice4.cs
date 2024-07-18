c3be9754c9b9913fae079dd9a6c9e129dcedae8e
cansunakipoglu <cansuunakipoglu@gmail.com>
2024-07-18 14:15:17 +0300

    Operatörler pratiğini tamamladım.

---
+++ Week1/Practices/Practice4.cs
@@ -0,0 +1,30 @@
+using Week1.Practices.Abscracts;
+
+namespace Week1.Practices;
+
+/// <summary>
+/// Operatörler
+/// </summary>
+public class Practice4:IPractice
+{
+    public void ExecPractice()
+    {
+        int x = 2;
+        int y = 5;
+        int toplam = x + y;
+        int carpim = x * y;
+        int bolum = x / y;
+        int fark = x - y;
+        int kalan = x % y;
+        Console.WriteLine(x > y);
+        
+        int z = 12;
+        int sonuc=z -= 5; 
+       
+       Console.WriteLine(sonuc);
+
+
+
+
+    }
+}
