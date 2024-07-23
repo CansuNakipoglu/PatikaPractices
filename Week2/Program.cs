5c24ff587ea9c91fcfc4e0203d9c49db5affbeba
cansunakipoglu <cansuunakipoglu@gmail.com>
2024-07-24 00:13:31 +0300

    Fix

---
+++ Week2/Program.cs
@@ -0,0 +1,20 @@
+﻿using Week2.Practices;
+using Week2.Practices.Abstracts;
+
+IPractice practice = null;
+
+if (practice == null)
+{
+    Console.Write("Lütfen Çalıştırmak İstediğiniz Uygulamanın Numarasını Giriniz: ");
+    var selection = Console.ReadLine();
+    
+    switch (selection)
+    {
+        case "1": practice = new Practice1();
+            break;
+        default: Console.WriteLine("Lütfen Geçerli Bir Uygulama Numarası Giriniz");
+            return;
+    }    
+}
+
+practice.ExecPractice();
